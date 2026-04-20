6dfae0645a3c9e9c299f24ae2030a623f81dc028
barrosmalberto <barros.alberto97@gmail.com>
2026-04-20 15:24:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ with st.sidebar:
     # --- SELETOR DE BASEMAP (APENAS NATIVOS) ---
     st.markdown("### 🗺️ Estilo do Mapa")
     estilos_mapa = {
-        "Claro Técnico (Padrão)": "light",
-        "Modo Escuro": "dark"
+        "Claro (Padrão)": "light",
+        "Modo Escuro": "dark",
+        "Híbrido/Estradas": "road"
     }
     mapa_selecionado = st.selectbox("Escolha a base de visualização:", list(estilos_mapa.keys()))
     basemap_pdk = estilos_mapa[mapa_selecionado]
